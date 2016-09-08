dfab7bd14d4d044968db5efeffa88cbb19110b67
Will McAuliff <womcauliff@gmail.com>
2016-09-07 23:55:38 -0400

    Word object modified to maintain a list of Letter objects

--- word.js
+++ word.js
@@ -1,8 +1,33 @@
-Word = function (word) {
-	var displaytext = word;
+//import Letter Object
+var Letter = require("./letter.js");
 
+Word = function (word) {
+	var actualValue = word;
+	var letterList = [];
+
+	//Parses word parameter into Letter Objects,
+	//adding them to internal array
+	for(var i = 0; i < word.length; i++) {
+		console.log(word[i]);
+		letterList[letterList.length] = new Letter([word[i]]);
+	}
+
+	/**
+	 * getDisplayText()
+	 *
+	 * Outputs the text for a game of hangman, with blanks
+	 * substituted for letters not yet correctly guessed.
+	 *
+	 * @return {string} the text for a game of hangman, with blanks
+	 * substituted for letters not yet correctly guessed.
+	 */
 	this.getDisplayText = function() {
-		return displaytext;
+		console.log("Word.getDisplayText()");
+		var displayText = "";
+		for(var i = 0; i < letterList.length; i++) {
+			displayText += letterList[i].getDisplayText() + " ";
+		}
+		return displayText;
 	}
 }
 
