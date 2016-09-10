ee8ecc9083c17c799cc53585e4a5575c5dab978b
Will McAuliff <womcauliff@gmail.com>
2016-09-09 20:25:56 -0400

    Inquirer package accepts input, and validates. Recursive use of game in main.

--- main.js
+++ main.js
@@ -1,12 +1,54 @@
 
 var Game = require("./game.js");
-
+var inquirer = require("inquirer");
 var GameController = new Game();
-GameController.buildList('./possible_words.txt');
+GameController.buildList(
+	'./possible_words.txt',
+	function(err) {
+		console.log("buildList callback()");
+		if(err) {
+			console.log(err);
+			return;
+		}
 
-GameController.selectWord();
-console.log(GameController.getSecretWord().getDisplayText());
-GameController.selectWord();
-console.log(GameController.getSecretWord().getDisplayText());
-GameController.selectWord();
-console.log(GameController.getSecretWord().getDisplayText());
+		GameController.selectWord(function(err) {
+			console.log("selectWord callback()")
+			if(err) {
+				console.log(err);
+				return;
+			}
+			console.log("word is selected. game beginning.");
+			gameRound();
+		});
+	}
+);
+
+function gameRound() {
+	console.log("gameRound()");
+	inquirer.prompt([
+		{
+			name: "letterGuess",
+			message: "Guess a letter:",
+			type: "input",
+			validate: function(value) {
+				if (value.length !== 1) {
+					console.log("\nPlease enter a single letter as your guess.");
+					return false;
+				}
+				else if (!/[a-zA-Z]/.test(value)) {
+					console.log("\nPlease enter a letter from the alphabet as your guess.");
+					return false;
+				}
+				else if(!GameController.isNewGuess(value)) {
+					console.log("\nYou have already guessed " + value 
+						+ "\nPlease guess a different letter.");
+					return false;
+				}
+				return true;
+			}
+		}
+	]).then(function (answer){
+		console.log(answer.letterGuess);
+		gameRound();
+	});
+}
